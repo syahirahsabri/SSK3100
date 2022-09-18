3b44878093bccac539226881b5475589cd736d7b
syahirahsabri <78020060+syahirahsabri@users.noreply.github.com>
2022-09-18 19:12:06 +0800

    Add files via upload

---
+++ Project week 1/src/Welcome_to_Java.java
@@ -0,0 +1,24 @@
+//Author's name: Noor Syahirah
+//Matric number: 207542
+//Assignment No.: 1
+
+public class Welcome_to_Java {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to Java.");
+		System.out.println(" Programming is fun. ");
+		
+		System.out.println();
+		
+		System.out.println("*****");
+		
+		System.out.println("Learning Java Now.");
+		
+		System.out.println();
+		
+		System.out.println("####");
+
+	}
+
+}
